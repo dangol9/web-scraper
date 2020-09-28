6c770da583b55ff9d8e16785d0e50bcbeb17f4a2
Daniel Golubkov <43164744+dangol9@users.noreply.github.com>
2020-09-28 12:25:31 +0300

    Styling edits

--- scraping.py
+++ scraping.py
@@ -35,7 +35,7 @@ for i in range(1, 0):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     sleep(1)
 
-#python -i scraping.py
+#python scraping.py
 
 links = driver.find_elements_by_class_name('simple_fit_item')
 
@@ -55,17 +55,21 @@ group_names_table = []
 i = 0
 
 for profile in profiles:
+
     driver.get(profile)
 
     sleep(2)
 
     print(profile)
+
     profile_names = driver.find_elements_by_xpath('//h2[@class="op_header"]')
     profile_names.pop(0)
 
 
     for profile_name in profile_names:
+
         print(profile_name.text)
+
         profile_names_table.append(profile_name.text)
 
     more_info_btn = driver.find_element_by_xpath('//*[@class="OwnerInfo__linkBold"]')
@@ -86,6 +90,7 @@ for profile in profiles:
 
         #    driver.execute_script("window.scrollTo(0,document.documentElement.scrollHeight);")
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
         sleep(1)
 
         #    new_height = driver.execute_script("return document.documentElement.scrollHeight")
@@ -101,31 +106,40 @@ for profile in profiles:
         sleep(1)
 
         if not group_names:
+
             print("Groups not available")
             group_names_table.insert(i, ["Groups not available"])
             #group_names_table.append("Groups not available")
             i += 1
+
         else:
+
             listing = []
+
             for group_name in group_names:
                 listing.append(group_name.text.split("\n")[0])
                 print(group_name.text.split("\n")[0])
+
                 #group_names_table.insert(i, [listing])
+                #
                 print(i)
+
                 print(listing)
+
             group_names_table.insert(i, listing)
+
             print(group_names_table)
+
             i += 1
-            #df.append({'C': listing}, ignore_index=True)
-            #df.to_excel('result.xlsx', index = False)
-                #group_names_table.append(group_name.text.split("\n")[0])
+
         print("\n")
+
         sleep(1)
 
     except:
         print("Groups not available")
-        #group_names_table.append("Groups not available")
         group_names_table.insert(i, ["Groups not available"])
+
         i += 1
     #error catch ends here
 

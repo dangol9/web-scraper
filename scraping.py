871e935cbe3eb691ab9a1307ca2468e905e40763
Daniel Golubkov <43164744+dangol9@users.noreply.github.com>
2020-09-28 12:20:10 +0300

    Added upload to excel file

--- scraping.py
+++ scraping.py
@@ -4,6 +4,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from secrets import username, password
+import pandas as pd
+
+df = pd.DataFrame()
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 options = webdriver.ChromeOptions()
@@ -28,7 +31,7 @@ driver.get("https://m.vk.com/search?c[section]=people&c[group]=9884911")
 
 sleep(1)
 
-for i in range(1, 6):
+for i in range(1, 0):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     sleep(1)
 
@@ -45,6 +48,12 @@ for link in links:
 
 sleep(1)
 
+df['Profile'] = profiles
+
+profile_names_table = []
+group_names_table = []
+i = 0
+
 for profile in profiles:
     driver.get(profile)
 
@@ -54,34 +63,36 @@ for profile in profiles:
     profile_names = driver.find_elements_by_xpath('//h2[@class="op_header"]')
     profile_names.pop(0)
 
+
     for profile_name in profile_names:
         print(profile_name.text)
+        profile_names_table.append(profile_name.text)
 
     more_info_btn = driver.find_element_by_xpath('//*[@class="OwnerInfo__linkBold"]')
     more_info_btn.click()
 
     sleep(2)
 
-    # add error catch 
+    # add error catch
     try:
         groups_btn = driver.find_element_by_xpath("//div[contains(text(), 'Following')]")
         groups_btn.click()
 
         sleep(1)
 
-        last_height = driver.execute_script("return document.documentElement.scrollHeight")
+        #last_height = driver.execute_script("return document.documentElement.scrollHeight")
 
-        while True:
+        #while True:
 
-            driver.execute_script("window.scrollTo(0,document.documentElement.scrollHeight);")
+        #    driver.execute_script("window.scrollTo(0,document.documentElement.scrollHeight);")
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        sleep(1)
 
-            sleep(1)
+        #    new_height = driver.execute_script("return document.documentElement.scrollHeight")
 
-            new_height = driver.execute_script("return document.documentElement.scrollHeight")
-
-            if new_height == last_height:
-                break
-            last_height = new_height
+        #    if new_height == last_height:
+        #        break
+        #    last_height = new_height
 
         sleep(1)
 
@@ -91,14 +102,36 @@ for profile in profiles:
 
         if not group_names:
             print("Groups not available")
+            group_names_table.insert(i, ["Groups not available"])
+            #group_names_table.append("Groups not available")
+            i += 1
         else:
+            listing = []
             for group_name in group_names:
+                listing.append(group_name.text.split("\n")[0])
                 print(group_name.text.split("\n")[0])
+                #group_names_table.insert(i, [listing])
+                print(i)
+                print(listing)
+            group_names_table.insert(i, listing)
+            print(group_names_table)
+            i += 1
+            #df.append({'C': listing}, ignore_index=True)
+            #df.to_excel('result.xlsx', index = False)
+                #group_names_table.append(group_name.text.split("\n")[0])
         print("\n")
         sleep(1)
 
     except:
         print("Groups not available")
+        #group_names_table.append("Groups not available")
+        group_names_table.insert(i, ["Groups not available"])
+        i += 1
     #error catch ends here
+
+df['Name'] = profile_names_table
+df['Groups'] = group_names_table
+
+df.to_excel('result.xlsx', index = False)
 
 driver.quit()

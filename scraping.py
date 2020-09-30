3c9d300449a03955e9b3dfc94af2b731fc3419f0
Daniel Golubkov <43164744+dangol9@users.noreply.github.com>
2020-09-30 19:49:53 +0300

    Changed page where the data is taken from and also added few exceptions for more stable work

--- scraping.py
+++ scraping.py
@@ -4,11 +4,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from secrets import username, password
+from selenium.common import exceptions
 import pandas as pd
 
 df = pd.DataFrame()
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
+
 options = webdriver.ChromeOptions()
 options.add_argument('--disable notifications')
 
@@ -27,31 +29,71 @@ login_btn.click()
 
 sleep(2)
 
-driver.get("https://m.vk.com/search?c[section]=people&c[group]=9884911")
+#driver.get("https://m.vk.com/search?c[section]=people&c[group]=9884911")
+driver.get("https://m.vk.com/search?c[section]=people&c[group]=178161127&offset=200")
 
 sleep(1)
 
-for i in range(1, 0):
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    sleep(1)
+links = []
+profiles = []
+
+while True:
+
+    for i in range(1, 3):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        sleep(1)
+
+    try:
+
+        links = driver.find_elements_by_class_name('simple_fit_item')
+
+        if not links:
+
+            break
+
+        else:
+                for link in links:
+                    profiles.append(link.get_attribute('href'))
+
+                more_users_btn = driver.find_element_by_xpath('//a[@class="show_more"]')
+                more_users_btn.click()
+                
+                sleep(1)
+
+    except (exceptions.StaleElementReferenceException, exceptions.NoSuchElementException) as e:
+        pass
+        break
+
+#last_height = driver.execute_script("return document.documentElement.scrollHeight")
+
+#while True:
+
+#    driver.execute_script("window.scrollTo(0,document.documentElement.scrollHeight);")
+    #driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+#    sleep(1)
+
+#    new_height = driver.execute_script("return document.documentElement.scrollHeight")
+
+#    if new_height == last_height:
+#        break
+#    last_height = new_height
+
+sleep(1)
 
 #python scraping.py
 
-links = driver.find_elements_by_class_name('simple_fit_item')
-
-profiles = []
-
 sleep(1)
-
-for link in links:
-    profiles.append(link.get_attribute('href'))
 
 sleep(1)
 
 df['Profile'] = profiles
 
 profile_names_table = []
+
 group_names_table = []
+
 i = 0
 
 for profile in profiles:
@@ -65,39 +107,29 @@ for profile in profiles:
     profile_names = driver.find_elements_by_xpath('//h2[@class="op_header"]')
     profile_names.pop(0)
 
-
-    for profile_name in profile_names:
-
-        print(profile_name.text)
-
-        profile_names_table.append(profile_name.text)
-
-    more_info_btn = driver.find_element_by_xpath('//*[@class="OwnerInfo__linkBold"]')
-    more_info_btn.click()
-
-    sleep(2)
-
-    # add error catch
+# add error catch
     try:
+
+        for profile_name in profile_names:
+
+            print(profile_name.text)
+
+            profile_names_table.append(profile_name.text)
+
+        more_info_btn = driver.find_element_by_xpath('//*[@class="OwnerInfo__linkBold"]')
+        more_info_btn.click()
+
+        sleep(2)
+
         groups_btn = driver.find_element_by_xpath("//div[contains(text(), 'Following')]")
         groups_btn.click()
 
         sleep(1)
 
-        #last_height = driver.execute_script("return document.documentElement.scrollHeight")
-
-        #while True:
-
         #    driver.execute_script("window.scrollTo(0,document.documentElement.scrollHeight);")
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
         sleep(1)
-
-        #    new_height = driver.execute_script("return document.documentElement.scrollHeight")
-
-        #    if new_height == last_height:
-        #        break
-        #    last_height = new_height
 
         sleep(1)
 
@@ -121,14 +153,14 @@ for profile in profiles:
                 print(group_name.text.split("\n")[0])
 
                 #group_names_table.insert(i, [listing])
-                #
+
                 print(i)
 
-                print(listing)
+                #print(listing)
 
             group_names_table.insert(i, listing)
 
-            print(group_names_table)
+            #print(group_names_table)
 
             i += 1
 
@@ -136,12 +168,18 @@ for profile in profiles:
 
         sleep(1)
 
+    #error catch ends here
     except:
+
         print("Groups not available")
         group_names_table.insert(i, ["Groups not available"])
 
+        print(i)
+
         i += 1
-    #error catch ends here
+
+        print("\n")
+
 
 df['Name'] = profile_names_table
 df['Groups'] = group_names_table

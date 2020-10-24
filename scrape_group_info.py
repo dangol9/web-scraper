22f35d486da9eb6e4e09421e3adc4901fb01dccd
Daniel Golubkov <43164744+dangol9@users.noreply.github.com>
2020-10-24 14:37:57 +0300

    Workflow changes

--- scrape_group_info.py
+++ scrape_group_info.py
@@ -12,15 +12,20 @@ df = pd.DataFrame()
 
 groups_name_table = []
 
-group_followers_table = []
+groups_about_table = []
 
+groups_followers_table = []
+
+df2 = pd.DataFrame()
+
+c = 0
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 options = webdriver.ChromeOptions()
 options.add_argument('--disable notifications')
 
-driver.get("https://vk.com")
+driver.get("https://m.vk.com")
 
 sleep(1)
 
@@ -41,38 +46,64 @@ df = pd.read_excel('TopGroup.xlsx')
 groups = df['Group'].tolist()
 
 sleep(1)
-
+groups = groups[:20]
+print(groups)
 for group in groups:
+    c += 1
+
     group_in = driver.find_element_by_xpath("//input[@placeholder='Search communities']")
     group_in.send_keys(group)
 
     sleep(2)
 
+    search_btn = driver.find_element_by_xpath("//button[contains(@class, 'ui_search_button_search _ui_search_button_search')]")
+
+    search_btn.click()
+
+    sleep(2)
+
     try:
-        link = driver.find_element_by_xpath("//div[contains(@class, 'labeled title')]/a").get_attribute('href')
-        driver.get(link)
+        about = driver.find_element_by_xpath("//div[contains(@class, 'labeled')][2]")
+        print("About: " + about.text)
+        groups_about_table.append(about.text)
     except:
-        print("Wierd search")
-    sleep(1)
-
-    print("Name: "+ str(group))
-
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-    sleep(1)
+        print("No about info")
+        groups_about_table.append("No about info")
 
     try:
-        followers = driver.find_element_by_xpath("//div[@id='public_followers']/a[@class='module_header']/div[contains(@class, 'header_top clear_fix')]")
-        print("Followers: " + followers.text.split("\n")[1])
+        name = driver.find_element_by_xpath("//div[contains(@class, 'labeled title')]")
+        link = driver.find_element_by_xpath("//div[contains(@class, 'labeled title')]/a").get_attribute('href')
+        print("Name: "+ name.text)
+        groups_name_table.append(name.text)
+        #driver.get(link)
+    except:
+        print("Bad search")
+        groups_name_table.append("Bad search")
+
+    #driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    #sleep(2)
+
+    try:
+        #followers = driver.find_element_by_xpath("//div[@id='public_followers']/a[@class='module_header']/div[contains(@class, 'header_top clear_fix')]")
+        followers = driver.find_element_by_xpath("//div[contains(@class, 'labeled')][3]")
+        print("Followers: " + followers.text.split(" ")[0])
+        #print("Followers: " + follower.text.split("\n")[1])
+        groups_followers_table.append(followers.text.split(" ")[0])
     except:
         print("Followers not available")
+        groups_followers_table.append("Followers not available")
 
-    try:
-
-        status = driver.find_element_by_xpath("//div[contains(@class, 'page_current_info')]/span[contains(@class, 'current_text')]")
-        print("Status: " + status.text)
-    except:
-        print("Status not available")
+    print(c)
 
     driver.get("https://vk.com/groups?act=catalog&c%5Blike_hints%5D=1&c%5Bper_page%5D=40&c%5Bsection%5D=communities&c%5Bskip_catalog%5D=1&c%5Bsort%5D=6")
-    sleep(1)
+
+    sleep(2)
+
+df2['Name']= groups_name_table
+df2['About'] = groups_about_table
+df2['Followers'] = groups_followers_table
+
+df2.to_excel('result_groups.xlsx', index = False)
+
+driver.quit()
